1ae26daa562fec0ab53129984b9425e30814e9d7
liyangbj <liyang@vip.ubeii.com>
2019-08-14 19:10:20 +0800

    spring cloud  eureka集成练习

--- src/main/java/com/liy/cloud/controller/SystemRoleController.java
+++ src/main/java/com/liy/cloud/controller/SystemRoleController.java
@@ -4,8 +4,11 @@ import com.liy.cloud.enttiy.SystemRole;
 import com.liy.cloud.service.SystemRoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -16,6 +19,9 @@ public class SystemRoleController {
     @Autowired
     private SystemRoleService systemRoleService;
 
+    // 声明服务发现客户端
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @PostMapping("/save")
     public SystemRole saveObject(@RequestBody SystemRole role){
         return systemRoleService.saveObject(role);
@@ -28,6 +34,30 @@ public class SystemRoleController {
     @PostMapping("/getAll")
     public List<SystemRole> getAll(){
         return systemRoleService.getSystemRoleList();
+    }
+
+
+    /**
+     *  获取服务列表中所有的服务名称
+     * @return
+     */
+    @GetMapping("/discovery")
+    public Object discoveryClientConfiguration(){
+
+        // 获取所有服务列表中服务名称，即：spring.application.name
+        List<String> services = discoveryClient.getServices();
+        services.forEach(service ->{
+            // 获取指定名称的所有服务提供者
+            List<ServiceInstance> serviceInstances = discoveryClient.getInstances(service);
+            serviceInstances.forEach(serviceInstance -> {
+                String host = serviceInstance.getHost();
+                URI uri = serviceInstance.getUri();
+                String serviceId = serviceInstance.getServiceId();
+                int port = serviceInstance.getPort();
+                log.info("host[{}],port[{}],uri[{}],serviceId[{}]",host,port,uri,serviceId);
+            });
+        });
+        return services;
     }
 
 }
